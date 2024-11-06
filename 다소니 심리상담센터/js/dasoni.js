72217027a927bc9656adbabab63e5396c7c43105
kimminseo16 <superbaby15@naver.com>
2024-11-06 13:59:28 +0900

    dasoni

--- 다소니 심리상담센터/js/dasoni.js
+++ 다소니 심리상담센터/js/dasoni.js
@@ -2,19 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
     let btns = document.querySelectorAll('.box')
     let slide1 = document.querySelector('.s1')
     let slide2 = document.querySelector('.s2')
+    let pText1 = document.querySelectorAll('ptext1')
 
     function changeSlide(){
         slide1.classList.toggle('on')
         slide2.classList.toggle('on')
-    
+        pText1.classList.toggle('on')
+        
     }
-    autoSlide = setInterval(changeSlide,3000)
+    autoSlide = setInterval(changeSlide,6000)
 
     btns.forEach(btn => {
         btn.addEventListener('click',function(){
         changeSlide();
         clearInterval(autoSlide);
-        autoSlide = setInterval(changeSlide,3000)
+        autoSlide = setInterval(changeSlide,6000)
     })
     }); 
 })
